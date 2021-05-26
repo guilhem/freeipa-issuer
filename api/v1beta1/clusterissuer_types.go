d2d33dd10b7d8550caa81f3f58984fa3c300c052
Yusuf Koer <ykoer@redhat.com>
2021-05-26 10:06:40 +0200

    Fix issue with clusterissuer inheritance from issuer

--- api/v1beta1/clusterissuer_types.go
+++ api/v1beta1/clusterissuer_types.go
@@ -16,17 +16,31 @@ limitations under the License.
 
 package v1beta1
 
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Cluster
 
 // ClusterIssuer is the Schema for the clusterissuers API
-type ClusterIssuer Issuer
+type ClusterIssuer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   IssuerSpec   `json:"spec,omitempty"`
+	Status IssuerStatus `json:"status,omitempty"`
+}
 
 // +kubebuilder:object:root=true
 
 // ClusterIssuerList contains a list of ClusterIssuer
-type ClusterIssuerList IssuerList
+type ClusterIssuerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterIssuer `json:"items"`
+}
 
 func init() {
 	SchemeBuilder.Register(&ClusterIssuer{}, &ClusterIssuerList{})
